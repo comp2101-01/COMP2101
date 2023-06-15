539e840ed18d2ec4e5a196bdb7c907ff8cfa5a20
Dennis Simpson <comp2101-01@zonzorp.net>
2023-06-15 16:37:28 -0400

    demos added

--- bash/text-to-array.sh
+++ bash/text-to-array.sh
@@ -6,4 +6,7 @@ users="$(cut -d: -f1 /etc/passwd)"
 while read; do
 	myarray+=("$REPLY")
 done <<< "$users"
-echo "myarray has ${#myarray[@]} elements, with '${myarray[0]}' as the first element"
+echo "myarray has ${#myarray[@]} elements"
+for (( index=0; index < ${#users[@]}; index++ )); do
+	echo index $index is ${users[$index]}
+done
